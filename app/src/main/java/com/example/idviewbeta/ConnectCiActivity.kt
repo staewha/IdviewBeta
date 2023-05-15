78572467d6018b5e3b18390837905906b8c5217d
Junghye Choi <65644118+bigtreeaa@users.noreply.github.com>
2023-05-15 21:51:59 +0900

    last update : 2023-05-15

---
+++ app/src/main/java/com/example/idviewbeta/ConnectCiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.idviewbeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectCiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect_ci)
+    }
+}
